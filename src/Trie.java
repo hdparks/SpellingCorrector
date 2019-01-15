0c18136116903e552c7b6d623376da4ba38aab98
Hayden Parkinson <haydendparkinson@gmail.com>
2019-01-15 15:42:54 -0700

    first commit

---
+++ src/Trie.java
@@ -0,0 +1,20 @@
+public class Trie implements spell.ITrie {
+
+    public void add(String word) {
+
+    }
+
+    public INode find(String word) {
+        return null;
+    }
+
+    public int getWordCount() {
+        return 0;
+    }
+
+    public int getNodeCount() {
+        return 0;
+    }
+
+
+}
